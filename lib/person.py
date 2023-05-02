17ef44e6361bfc032d06d33d2fbd64eb0ef12043
Peter Nordel <peternordel@gmail.com>
2023-05-02 16:29:13 -0600

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    def __init__(self, name = "Peter", job = "Admin"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        print("Retrieving name")
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and 0 < len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        print("Retrieving job")
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+peter = Person("peter", "Admin")
+print(peter.job, peter.name)
